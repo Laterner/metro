07f893a530af67c5e9a1891c50e2c2a80f9b81b8
Laterner <ivantrofimov2001@gmail.com>
2023-06-07 16:48:59 +0300

    init admin

--- app/init_db.py
+++ app/init_db.py
@@ -6,27 +6,37 @@ def init():
     cur = conn.cursor()
 
     # Execute a command: this creates a new table
-    # cur.execute('DROP TABLE IF EXISTS users;')
+    cur.execute('DROP TABLE IF EXISTS users;')
+    cur.execute('CREATE TABLE users( \
+        id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY, \
+        firstname VARCHAR(63), \
+        lastname VARCHAR(63), \
+        email VARCHAR(63), \
+        password VARCHAR(63) \
+        role VARCHAR(15) \
+    );')
 
-    # cur.execute('CREATE TABLE users( \
-    #     id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY, \
-    #     firstname VARCHAR(63), \
-    #     lastname VARCHAR(63), \
-    #     email VARCHAR(63), \
-    #     password VARCHAR(63) \
-    #     role VARCHAR(15) \
-    # );')
+    cur.execute('DROP TABLE IF EXISTS requests;')
+    cur.execute('CREATE TABLE requests( \
+        id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY, \
+        firstname VARCHAR(63), \
+        lastname VARCHAR(63), \
+        email VARCHAR(63), \
+        station VARCHAR(31), \
+        request_text TEXT, \
+        files VARCHAR(127) \
+    );')
 
-    # cur.execute('DROP TABLE IF EXISTS requests;')
-    # cur.execute('CREATE TABLE requests( \
-    #     id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY, \
-    #     firstname VARCHAR(63), \
-    #     lastname VARCHAR(63), \
-    #     email VARCHAR(63), \
-    #     station VARCHAR(31), \
-    #     request_text TEXT, \
-    #     files VARCHAR(127) \
-    # );')
+    cur.execute('INSERT INTO users \
+            ( firstname, lastname, email, password )'
+            'VALUES (%s, %s, %s, %s);',
+            (
+                'firstname', 
+                'lastname', 
+                'admin@mail.ru', 
+                'password'
+            )
+    )
 
     conn.commit()
 
